8b7d205281ad018057ffa5c12b27b7146e02c8a7
ajaysagarn <ajaysagarn@gmail.com>
2021-12-04 13:50:27 -0600

    [Update] - Add logging and comments

--- MonitoringService/src/main/scala/kafka/MonitorKafkaProducer.scala
+++ MonitoringService/src/main/scala/kafka/MonitorKafkaProducer.scala
@@ -27,13 +27,25 @@ class MonitorKafkaProducer(actorSystem: ActorSystem) {
   private val producerConfig: Config =  system.settings.config.getConfig("akka.kafka.producer")
   private val topic = config.getString("kafka.topic")
 
+  logger.info("Get the bootstrap server strings for the kafka cluster")
   val bootstrapServers = config.getString("kafka.bootstrapServers")
+
+  logger.info("Configure the producer instance")
   val producerSettings =  ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
     .withBootstrapServers(bootstrapServers)
 
   val producer = SendProducer(producerSettings)
 
+  /**
+   * Function to publish messages to Kafka.
+   * Uses Akka Streams to construct a flow from the list of messages
+   * Uses Kafka Producer Sink provided by Alpakka Kafka to connect to Kafka
+   * @param logs - List of logs to be published
+   * @param filename - The name of the file who's logs are being published
+   */
   def publishToKafka(logs: Vector[String],filename: String): Unit ={
+
+    logger.info("Creating kafka source")
     val source = Source.fromIterator(() => logs.iterator)
     val mapToKafkaMessage: Flow[String,LogMessage,NotUsed] =  Flow[String].map[LogMessage](log => {
       LogMessage(getLogTimeStamp(log).getOrElse(""),getLogLevel(log),getLogMessage(log),filename)
@@ -41,6 +53,7 @@ class MonitorKafkaProducer(actorSystem: ActorSystem) {
 
     val mapLogToJson: Flow[LogMessage, String, NotUsed] = Flow[LogMessage].map[String](log => log.toJson.prettyPrint)
 
+    logger.info("Akka Stream to publish source to the producer sink")
     val done: Future[Done] = source.via(mapToKafkaMessage)
       .via(mapLogToJson)
       .map(value => new ProducerRecord[String, String](topic, value))
